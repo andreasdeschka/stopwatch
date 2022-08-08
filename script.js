1fd656dd506eca3cf3c8482ca86f32ffec8f10ee
andreasdeschka <andreas.deschka@gmail.com>
2022-08-08 11:12:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function displayTimer() {
   time += miliseconds;
 
   T.timerDiv.innerHTML = time;
-  document.getElementById('extra').innerHTML = "Hours: " + Math.round(10*(hours + minutes/60 + seconds/3600))/10;
+  document.getElementById('hhdec').innerHTML = "Hours: " + Math.round(10*(hours + minutes/60 + seconds/3600))/10;
 }
 
 function startTimer() {
